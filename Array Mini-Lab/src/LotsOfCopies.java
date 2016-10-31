a0a47c9fcd05ab34ea7116edf43cadba758c5eae
takahiromollenkamp <takahiro.mollenkamp@gmail.com>
2016-10-31 09:46:50 -0700

    sync

--- Array Mini-Lab/src/LotsOfCopies.java
+++ Array Mini-Lab/src/LotsOfCopies.java
@@ -2,16 +2,7 @@
 public class LotsOfCopies {
 
 	public static void main(String[] args) {
-		int num=7;
-		String strMain="APCS";
-		int[] arrMain={1,2,3,4,5};
-		int b=num;
-		num=5;
 		
-	}
-	public static void changeMe(int x, String str, int [] arr){
-		x=2;
-		str="new thing";
-		arr[0]=500;
+		
 	}
 }
