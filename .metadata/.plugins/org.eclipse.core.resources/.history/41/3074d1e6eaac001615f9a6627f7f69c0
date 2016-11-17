088d3985c8f7d01b45fa8b0687f09dcf5c9a14c1
takahiromollenkamp <takahiro.mollenkamp@gmail.com>
2016-11-17 09:50:43 -0800

    s

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/41/3074d1e6eaac001615f9a6627f7f69c0
@@ -0,0 +1,10 @@
+package fracCalc;
+
+public class OldCode {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
--- FracCalc/src/fracCalc/OldCode.java
+++ FracCalc/src/fracCalc/OldCode.java
@@ -1,10 +1,20 @@
 package fracCalc;
 
+import java.util.Scanner;
+
 public class OldCode {
 
 	public static void main(String[] args) {
-		
+		Scanner input=new Scanner(System.in);//scanner
+    	String userInput=input.nextLine();
+    	String answer=produceAnswer(userInput);
+		System.out.println(answer);
 
 	}
+	 public static String produceAnswer(String input){
+		 String[] split = input.split(" ");
+		 return split[2];
+	 }
+	    
 
 }
