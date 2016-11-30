bb10a0a0bb4750f39ea4025afe8add91e9d36c68
takahiromollenkamp <takahiro.mollenkamp@gmail.com>
2016-11-30 11:29:12 -0800

    finish?
    do extracredit

--- FracCalc/src/fracCalc/OldCode.java
+++ FracCalc/src/fracCalc/OldCode.java
@@ -2,6 +2,100 @@ package fracCalc;
 
 import java.util.Scanner;
 
+/*
+  public static String produceAnswer(String input)
+    {   String answer="";
+    	String[] split = input.split(" ");//split to first operand, operator, and second operand
+        int[] firstOp=components(split[0]);
+        int[] secondOp=components(split[2]);
+        convertToImproper(firstOp);
+        convertToImproper(secondOp);
+        int[] finalArray=new int[2];
+        if (split[1].equals("+")){
+        	finalArray =add(firstOp, secondOp);
+        }
+        if (split[1].equals("-")){
+        	finalArray =subtract(firstOp, secondOp);
+        }
+        if (split[1].equals("*")){
+        	finalArray =multiply(firstOp, secondOp);
+        }
+        if (split[1].equals("/")){
+        	finalArray=divide(firstOp, secondOp);
+        }
+        answer=finalAnswer(finalArray);
+        return answer;
+    }
+    public static int[] add(int[] first, int[] second){
+    	int denominator = first[2]*second[2];
+    	first[1]*=second[2];
+    	second[1]*=first[2];
+    	int numerator=first[1]+second[1];
+    	int[] solution={numerator,denominator};
+    	return solution;
+    	//figure negative numbers
+    }
+    public static int[] subtract(int[] first, int[] second){
+    	int denominator = first[2]*second[2];
+    	first[1]*=second[2];
+    	second[1]*=first[2];
+    	int numerator=first[1]-second[1];
+    	int[] solution={numerator,denominator};
+    	return solution;
+    }
+    public static int[] multiply(int[] first, int[] second){
+    	int numerator=first[1]*second[1];
+    	int denominator=first[2]*second[2];
+    	int[] solution={numerator,denominator};
+    	return solution;
+    }
+    public static int[] divide(int[] first, int[] second){
+    	int a=second[1];
+    	int b=second[2];
+    	if(a<0){
+    		a=-a;
+    		b=-b;
+    	}
+    	int numerator=first[1]*b;
+    	int denominator=first[2]*a;
+    	int[] solution={numerator,denominator};
+    	return solution;
+    }
+ */
+public static int[] compute(int[] first, int[] second, String operator){
+	boolean add=operator.equals("+");
+	boolean subtract=operator.equals("-");
+	boolean multiply=operator.equals("*");
+	boolean divide=operator.equals("/");
+	int numerator=0;
+	int denominator=1;
+	if(add||subtract){
+		denominator = first[2]*second[2];
+    	first[1]*=second[2];
+    	second[1]*=first[2];
+    	if(subtract){
+    		second[1]*=-1;
+    	}
+    	numerator=first[1]+second[1];
+	}
+	if(multiply||divide){
+		if(divide){
+			int a=second[1];
+			int b=second[2];
+			second[2]=a;
+			second[1]=b;
+			if(a<0){
+				second[2]*=-1;
+				second[1]*=-1;
+			}
+		}
+		 numerator=first[1]*second[1];
+    	 denominator=first[2]*second[2];	
+	}
+	int[] solution={numerator,denominator};
+	return solution;
+}
+
 public class OldCode {
 
 	public static void main(String[] args) {
@@ -15,6 +109,7 @@ public class OldCode {
 		 String[] split = input.split(" ");
 		 return split[2];
 	 }
+}
 	    
 
 }
