d3cdbe692492ce9f40663a6b9580f0d64015ff32
takahiromollenkamp <takahiro.mollenkamp@gmail.com>
2017-03-01 11:29:43 -0800

    save

--- TextExcel/src/textExcel/RealCell.java
+++ TextExcel/src/textExcel/RealCell.java
@@ -1,19 +1,17 @@
 package textExcel;
 
-public class EmptyCell implements Cell {
+public class RealCell implements Cell {
 
 	@Override
 	public String abbreviatedCellText() {
 		// TODO Auto-generated method stub
-		return "";
+		return null;
 	}
 
 	@Override
 	public String fullCellText() {
 		// TODO Auto-generated method stub
-		return "";
+		return null;
 	}
-    public EmptyCell(){
-    	
-    }
+
 }
